bbf4cae934615d62b133bbb37e1912da5ca4e2ad
maktup <hasim419@gmail.com>
2021-10-06 23:13:20 +0900

    스테비아

--- 211001(211008)/js/mixin.js
+++ 211001(211008)/js/mixin.js
@@ -1,6 +1,6 @@
 window.onload = function() {
-  // 영상재생
   let mainV = document.querySelector('.main-v');
+  let exV = document.querySelector('.exper-v');
   let slide1 = document.querySelector('.swiper-slide1');
 
   function videoPlay() {
@@ -17,11 +17,24 @@ window.onload = function() {
     titFadeOut();
   }
 
+  function exvideoPlay() {
+    setTimeout(function() {
+      exV.play();
+    },1000);
+  }
+
+  function exvideoStop() {
+    setTimeout(function() {
+      exV.pause();
+      exV.currentTime = 0;
+    },500);
+  }
+
   let intit1;
   let intit2;
   function activeTit() {
-    intit1 = setTimeout(titFadeIn, 5000, 'active-t1');
-    intit2 = setTimeout(titFadeIn, 11000, 'active-t2');
+    intit1 = setTimeout(titFadeIn, 500, 'active-t1');
+    intit2 = setTimeout(titFadeIn, 10000, 'active-t2');
   }
 
   function titFadeIn(v) {
@@ -46,15 +59,18 @@ window.onload = function() {
   }
 
   mainV.addEventListener('ended', pageCheck);
+  exV.addEventListener('ended', exvideoPlay);
 
+  let slide8 = document.querySelector('.swiper-slide8 iframe');
 
-  let pageSwiper = document.querySelector('.pageSwiper');
+  let container = document.querySelector('.container');
   let nowPage = 1;
   swiper.on('slideChange', function() {
     nowPage = swiper.realIndex + 1;
     pageCheck();
     txtMotion(nowPage);
   });
+
   function pageCheck() {
     if(nowPage !== 1) {
       videoStop();
@@ -63,12 +79,46 @@ window.onload = function() {
     if(nowPage === 1) {
       videoPlay();
     }
+    if(nowPage === 4) {
+      exvideoPlay();
+    }
+    if(nowPage !== 4) {
+      exvideoStop();
+    }
+    if(nowPage === 8) {
+      utvPlay();
+    }
+    if(nowPage !== 8) {
+      utvStop();
+    }
+    if(nowPage === 12) {
+      setTimeout(function() {
+        scrollHide();
+      },500);
+    }
+    if(nowPage !== 12) {
+      setTimeout(function () {
+        scrollActive();
+      },500)
+    }
   }
 
   pageCheck();
   function txtMotion(v) {
-    pageSwiper.setAttribute('data-page',v);
+    container.setAttribute('data-page',v);
   }
+
+  function utvPlay() {
+    setTimeout(function() {
+      slide8.contentWindow.postMessage('{"event":"command","func":"' + 'playVideo' + '","args":""}', '*');
+    },1000);
+  }
+  function utvStop() {
+    setTimeout(function() {
+      slide8.contentWindow.postMessage('{"event":"command","func":"' + 'stopVideo' + '","args":""}', '*');
+    },500);
+  }
+
 
   // let i = 0;
   // let txt1 = '#훅가고싶니?';
@@ -95,4 +145,15 @@ window.onload = function() {
   //   }
   // }
 
+  let scrollDown = document.querySelector('.scroll-down');
+  scrollDown.addEventListener('click', function() {
+    swiper.slideTo(nowPage);
+  });
+
+  function scrollActive() {
+    scrollDown.classList.add('active');
+  }
+  function scrollHide() {
+    scrollDown.classList.remove('active');
+  }
 }
